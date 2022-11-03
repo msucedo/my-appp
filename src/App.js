83471f982fae5ea36c5f98f39ae4fcaeec224af9
Mario Saucedo <mario_rsa@hotmail.com>
2022-11-02 19:32:33 -0600

    search filtered feature

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+const week = [
+  { id: 1, name: 'mon', cool: 8 },
+  { id: 2, name: 'fri', cool: 10},
+  { id: 3, name: 'sun', cool: 9}
+]
+
+// JSX
+function MyButton({ count, onClick}) {
+
+  return (
+    <button onClick={onClick}> clickec { count } times</button>
+  );
+}
+
+function myComponent({ props1, props2 }) {
+  //logic
+  return (
+    //appareance
+    <>
+      <p></p>
+      <p></p>
+    </>
+  );
+}
 
 function App() {
+
+  //useState
+  const [count, setCount] = useState(0);
+
+  // event handler
+  function handleClick() {
+    // if else
+    count == 5 ? setCount(100) : setCount(count + 1)
+    // if
+    count == 101 && setCount(500)
+  }
+
+  //passing an array to a 'li' tag
+  const listDays = week.map( day => 
+    <li
+      key={day.id}
+      style={{ color:'red' }}
+    >
+      {day.name + "day"}
+    </li>
+  );
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ul>
+          {listDays}
+          <MyButton count={count} onClick={handleClick} />
+        </ul>
+        <MyButton count={count}  onClick={handleClick} />
         <a
           className="App-link"
           href="https://reactjs.org"
